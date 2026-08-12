d5f071fa9edc72ee59e49518d707e302967a6f6a
David Anthony <djanthony@gmail.com>
2026-08-11 21:33:10 -0500

    Adding graceful shutdown on SIGINT and SIGTERM from the command line (#80)

--- src/log_database_proxy_model.cpp
+++ src/log_database_proxy_model.cpp
@@ -1,6 +1,6 @@
 // *****************************************************************************
 //
-// Copyright (c) 2015, Southwest Research Institute® (SwRI®)
+// Copyright (c) 2026, Southwest Research Institute® (SwRI®)
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with or without
